18ea816c9428c04a845ed36d0f5c3cbdf69e57aa
Nickolay Grigoriev <gnarus@inbox.ru>
2025-02-13 20:10:38 +0300

    2 commit

--- gazp_parser/spiders/GAZP.py
+++ gazp_parser/spiders/GAZP.py
@@ -3,7 +3,7 @@ import scrapy
 
 class GazpSpider(scrapy.Spider):
     name = "GAZP"
-    allowed_domains = ["www.finam.ru"]
+    allowed_domains = ["www.finam.ru"] # Перечень домейнов на которые паук может зайти
     start_urls = ["http://www.finam.ru/"]
 
     def parse(self, response):
